1621816edcaaccd7e6a7bdbe3a6049cd01a3eb14
evertonsouls <evertonsouls@gmail.com>
2022-03-14 22:09:15 +0100

    Add service test

---
+++ tests/unit/server/service.test.js
@@ -0,0 +1,10 @@
+import { jest, expect, describe, test, beforeEach } from '@jest/globals'
+
+describe('#Services - test service for api', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+  })
+
+  test.todo()
+})
